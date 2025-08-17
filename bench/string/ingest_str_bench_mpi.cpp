58776a8642becb78f0daf9b6592924e6c30e0218
Karim Yasser Mohamed Yousr Youssef <youssef2@tuolumne1001.llnl.gov>
2025-08-17 15:17:35 -0700

    add mpi example

---
+++ bench/string/ingest_str_bench_mpi.cpp
@@ -0,0 +1,97 @@
+// Copyright 2025 Lawrence Livermore National Security, LLC and other Metall
+// Project Developers. See the top-level COPYRIGHT file for details.
+//
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+#include <mpi.h>
+
+#include <metall/metall.hpp>
+#include <metall/container/string.hpp>
+#include <metall/detail/time.hpp>
+#include <metall/utility/open_mp.hpp>
+
+#include <faker-cxx/faker.h>
+
+namespace mdtl = metall::mtlldetail;
+
+int main(int argc, char *argv[]) {
+
+  MPI_Init(&argc, &argv);
+
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <path> <num_chars_to_generate>"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  std::filesystem::path path = std::string(argv[1]) + "_" + std::to_string(world_rank);
+  std::size_t num_chars_to_generate = std::stoull(argv[2]);
+
+  std::unique_ptr<metall::manager> manager(
+      new metall::manager(metall::create_only, path));
+
+  auto ingest_str = [&](const std::string &str) {
+    char *buf = reinterpret_cast<char *>(manager->allocate(str.size() + 1));
+    std::copy(str.begin(), str.end(), buf);
+    buf[str.size()] = '\0';
+  };
+
+  const auto ingest_start_time = mdtl::elapsed_time_sec();
+  // OMP_DIRECTIVE(parallel) {
+    /* const int num_threads = metall::utility::omp::get_num_threads();
+
+    std::size_t n = num_chars_to_generate / num_threads; */ 
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    std::size_t n = num_chars_to_generate / world_size;
+
+    std::size_t cnt = 0;
+
+    while (cnt < n) {
+      const auto id = faker::string::uuidV4();
+      const auto email = faker::internet::email();
+      const auto city = faker::location::city();
+      const auto streetAddress = faker::location::streetAddress();
+
+      ingest_str(id);
+      cnt += id.size();
+
+      ingest_str(email);
+      cnt += email.size();
+
+      ingest_str(city);
+      cnt += city.size();
+
+      ingest_str(streetAddress);
+      cnt += streetAddress.size();
+    }
+  // }
+  const auto ingest_end_time = mdtl::elapsed_time_sec(ingest_start_time);
+  std::cout << "Ingested " << num_chars_to_generate << " characters in "
+            << ingest_end_time << " seconds." << std::endl;
+
+  const auto metall_sync_start_time = mdtl::elapsed_time_sec();
+  manager->flush();
+  const auto metall_sync_end_time =
+      mdtl::elapsed_time_sec(metall_sync_start_time);
+  std::cout << "Flush Metall manager in " << metall_sync_end_time << " seconds."
+            << std::endl;
+
+  const auto metall_close_start_time = mdtl::elapsed_time_sec();
+  manager.reset(nullptr);
+  const auto metall_close_end_time =
+      mdtl::elapsed_time_sec(metall_close_start_time);
+  std::cout << "Closed Metall manager in " << metall_close_end_time
+            << " seconds." << std::endl;
+
+  MPI_Finalize();
+
+  return EXIT_SUCCESS;
+}
